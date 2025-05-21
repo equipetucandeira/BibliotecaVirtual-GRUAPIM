70e5cec96d5359ffe87d161b3331044a3c5b11c7
JoaoWindowsBook <joaoruffino2005@gmail.com>
2025-05-21 18:24:07 -0300

    add: Model User

--- src/main/java/br/ifsp/library/model/User.java
+++ src/main/java/br/ifsp/library/model/User.java
@@ -1,5 +1,21 @@
 package br.ifsp.library.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "Name is required")
+	private String name;
+	@NotBlank(message = "Email is required")
+	private String email;
+	@NotBlank(message = "Password is required")
+	private String password;
 }
