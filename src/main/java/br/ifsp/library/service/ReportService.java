2ef856c4152037ba93164cff213ce5fdcd8c7462
JoaoWindowsBook <joaoruffino2005@gmail.com>
2025-06-04 21:51:01 -0300

    fix: some fixes

--- src/main/java/br/ifsp/library/service/ReportService.java
+++ src/main/java/br/ifsp/library/service/ReportService.java
@@ -1,10 +1,12 @@
 package br.ifsp.library.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.ifsp.library.dto.LibUseDTO;
 import br.ifsp.library.dto.MostBorrowedDTO;
 import br.ifsp.library.repository.ReservationRepository;
 
@@ -17,7 +19,7 @@ public class ReportService {
 	    return reservationRepository.findMostBorrowedBooks();
 	}
 	
-	/*public LibUseDTO reportLibUse(LocalDate startDate, LocalDate endDate) {
+	public LibUseDTO reportLibUse(LocalDate startDate, LocalDate endDate) {
 	    LibUseDTO dto = new LibUseDTO();
 	    dto.setTotalLoans(reservationRepository.countByStartDateBetween(startDate, endDate));
 	    dto.setTotalUsers(reservationRepository.countDistinctUsersByStartDateBetween(startDate, endDate));
@@ -26,5 +28,5 @@ public class ReportService {
 	    dto.setMostLoans(books.stream().findFirst().orElse(null));
 	    
 	    return dto;
-	}*/
+	}
 }
