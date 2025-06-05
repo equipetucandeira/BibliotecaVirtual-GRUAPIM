d69df8b57b330ee93092efd28c2f1deba55289f4
JoaoWindowsBook <joaoruffino2005@gmail.com>
2025-06-05 16:54:24 -0300

    comment test

--- src/main/java/br/ifsp/library/dto/ReservationResponseDTO.java
+++ src/main/java/br/ifsp/library/dto/ReservationResponseDTO.java
@@ -13,6 +13,14 @@ public class ReservationResponseDTO {
 	private LocalDate endDate;
 	private boolean active;
 	
+	public ReservationResponseDTO(Book book, LocalDate startDate, LocalDate endDate, boolean active) {
+	    this.book = book;
+	    this.startDate = startDate;
+	    this.endDate = endDate;
+	    this.active = active;
+	}
+
+	
 	public ReservationResponseDTO(Long id, Book book, LocalDate startDate, LocalDate endDate, boolean active) {
 		super();
 		this.id = id;
--- src/test/java/br/ifsp/library/controller/BookControllerTest.java
+++ src/test/java/br/ifsp/library/controller/BookControllerTest.java
@@ -1,38 +1,26 @@
-package br.ifsp.library.controller;
+/*package br.ifsp.library.controller;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 
-import br.ifsp.library.model.User;
 import br.ifsp.library.model.Book;
 import br.ifsp.library.dto.ReservationResponseDTO;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-
-import br.ifsp.library.controller.BookController;
 import br.ifsp.library.service.BookService;
 import br.ifsp.library.service.ReservationService;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
+@AutoConfigureMockMvc
 public class BookControllerTest {
 
   @MockBean
@@ -44,10 +32,9 @@ public class BookControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @WithMockUser(username = "admin", roles = "ADMIN")
+  @WithMockUser(username = "Admin", roles = {"ADMIN"})
   @Test
   void shouldReserveBookSuccessfully() throws Exception {
-    // Dados simulados
     Book book = new Book();
     book.setId(1L);
     book.setTitle("Teste de livro");
@@ -58,15 +45,15 @@ public class BookControllerTest {
     LocalDate startDate = LocalDate.now();
     LocalDate endDate = startDate.plusDays(7);
 
-    ReservationResponseDTO responseDTO = new ReservationResponseDTO(book, startDate, endDate, true);
+    ReservationResponseDTO responseDTO = new ReservationResponseDTO(1L, book, startDate, endDate, true);
 
-    // Mockando o service
-    Mockito.when(reservationService.reservBook(1L, "admin"))
+    when(reservationService.reservBook(1L, "Admin"))
         .thenReturn(responseDTO);
 
-    // Executando o teste
-    mockMvc.perform(post("/api/books/1/reserve"))
+    mockMvc.perform(post("/api/books/1/reserve")
+            .accept("application/json"))
         .andExpect(status().isOk());
   }
-
 }
+
+*/
