dbd9749e3431e43ab750be7343e383368f8c07d0
ThiagoMRibeiro <thiagomoralesribeiro@gmail.com>
2025-06-28 22:16:32 -0300

    feat: Adding the TC-001 and TC-002

---
+++ src/test/java/br/ifsp/library/manualTestsEvidence/TC-003/UserTestCatalogNavigationFilterFunctionalityClearLayout.java
@@ -0,0 +1,16 @@
+/*import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import br.ifsp.library.repository.BookRepository;
+import br.ifsp.library.repository.UserRepository;
+
+@SpringBootTest
+public class UserTestCatalogNavigationFilterFunctionalityClearLayout {
+  @MockBean
+  private BookRepository bookRepository;
+
+  @MockBean
+  private UserRepository userRepository;
+
+  
+}*/
